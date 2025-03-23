31e292480e4c21602621a005bfe62b7d13e66455
Jason <jsofokleous@googlemail.com>
2025-03-23 08:00:46 +0000

    added contrived form submission & routes

---
+++ app/routes/ErrorPage.tsx
@@ -0,0 +1,13 @@
+export default function OtherPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Error
+          </h1>
+        </header>
+      </div>
+    </div>
+  );
+}
