b8f4c3c2c1709a85b0913a8f001fd1d727da7c43
Jason <jsofokleous@googlemail.com>
2025-03-23 11:38:59 +0000

    integrate MSW mock service worker

--- server.js
+++ server.js
@@ -4,9 +4,28 @@ import express from 'express';
 import { createRequestHandler } from '@remix-run/express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const handlers = [
+  http.post('https://dummyjson.com/test', () => {
+    return HttpResponse.json({
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
+    })
+  }),
+]
+
+if(process.env.NODE_ENV === 'test') {
+  console.log('Starting MSW server');
+
+  const mockServer = setupServer(...handlers);
+  mockServer.listen();
+}
 
 // Import the build
 import * as build from './build/server/index.js';
