2eac5be664caf9c6baf08ce2f790cc24dd0c5f83
Jason <jsofokleous@googlemail.com>
2025-03-23 12:25:09 +0000

    Update smoke.cy.ts

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -2,10 +2,12 @@ describe('Submitting the form', () => {
   it('Redirects to the OtherPage on success', () => {
     cy.visit('/');
 
-    cy.get('input[name="name"]').type('Lilo Pelekai');
-    cy.get('input[name="email"]').type('test@example.com');
+    cy.get('input[name="title"]').type('Lilo Pelekai');
     cy.get('input[type="submit"]').click();
 
     cy.url().should('include', '/otherPage');
+
+    cy.get('[data-test-id="productId"]')
+      .should('have.text', 'from-mock-api-c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d');
   });
 });
